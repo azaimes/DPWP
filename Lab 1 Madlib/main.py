4e1752650a004a993512107e0d663a50d6b1020e
azaimes <azaimes@fullsail.edu>
2014-08-07 13:29:49 -0400

    added array

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -6,17 +6,26 @@ Name: Adam Zaimes
 start = 1
 while start != 0:
     #variables
-    name = raw_input("What is your name?")
-    hours = raw_input("How many hours do you spend studying?")
-    job = raw_input("What job do plan to get after graduation?")
-    gpa = raw_input("What is your current gpa?")
-    grad = raw_input("What year will you graduate?")
-    
+    name = raw_input("What is your name? ")
+    hours = raw_input("How many hours do you spend studying? ")
+    job = raw_input("What job do plan to get after graduation? ")
+    gpa = raw_input("What is your current gpa? ")
+    grad = raw_input("What year will you graduate? ")
+    age = raw_input("What year were you born? ")
+    #setup array
     relax = raw_input("What is your favorite video game?")
     relax2 = raw_input("What is your second favorite video game?")
+    #array to get input from above
+    games_played = []
+    #add to array
+    games_played.append(relax)
+    games_played.append(relax2)
 
 
 
 
-    
+
+
+
+
 
