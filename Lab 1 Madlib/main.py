1d36b0a37df0d3bb0687e9a0206f74162bba8fac
azaimes <azaimes@fullsail.edu>
2014-08-07 13:45:29 -0400

    tested and fixed issue with calculation
    added conversion to integer

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -1,17 +1,25 @@
 '''
 Name: Adam Zaimes
 '''
+#while loop to make the program continuous
 
-#intialize variable
 start = 1
-while start != 0:
+while start !=0:
+
     #variables
     name = raw_input("What is your name? ")
     hours = raw_input("How many hours do you spend studying? ")
     job = raw_input("What job do plan to get after graduation? ")
     gpa = raw_input("What is your current gpa? ")
-    grad = raw_input("What year will you graduate? ")
-    age = raw_input("What year were you born? ")
+    grad_year = raw_input("What year will you graduate? ")
+    born_year = raw_input("What year were you born? ")
+    #convert to integers for calculation
+    grad_year = int(grad_year)
+    born_year = int(born_year)
+    #calculate age at graduation
+    age = grad_year - born_year
+    print str(age)
+
     #setup array
     relax = raw_input("What is your favorite video game?")
     relax2 = raw_input("What is your second favorite video game?")
@@ -20,6 +28,8 @@ while start != 0:
     #add to array
     games_played.append(relax)
     games_played.append(relax2)
+
+
 
 
 
