20ce089df41379309d92f03f1fbb8e3201083667
azaimes <azaimes@fullsail.edu>
2014-08-07 13:21:49 -0400

    initial commit

---
+++ Lab 1 Madlib/main.py
@@ -0,0 +1,22 @@
+'''
+Name: Adam Zaimes
+'''
+
+#intialize variable
+start = 1
+while start != 0:
+    #variables
+    name = raw_input("What is your name?")
+    hours = raw_input("How many hours do you spend studying?")
+    job = raw_input("What job do plan to get after graduation?")
+    gpa = raw_input("What is your current gpa?")
+    grad = raw_input("What year will you graduate?")
+    
+    relax = raw_input("What is your favorite video game?")
+    relax2 = raw_input("What is your second favorite video game?")
+
+
+
+
+    
+
