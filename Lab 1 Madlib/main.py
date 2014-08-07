c98839d2244cfeba333469c3708237a8338c9383
azaimes <azaimes@fullsail.edu>
2014-08-07 13:59:28 -0400

    added function and tested

--- Lab 1 Madlib/main.py
+++ Lab 1 Madlib/main.py
@@ -13,21 +13,32 @@ while start !=0:
     gpa = raw_input("What is your current gpa? ")
     grad_year = raw_input("What year will you graduate? ")
     born_year = raw_input("What year were you born? ")
+
     #convert to integers for calculation
     grad_year = int(grad_year)
     born_year = int(born_year)
+
     #calculate age at graduation
     age = grad_year - born_year
-    print str(age)
 
     #setup array
     relax = raw_input("What is your favorite video game?")
     relax2 = raw_input("What is your second favorite video game?")
+
     #array to get input from above
     games_played = []
+
     #add to array
     games_played.append(relax)
     games_played.append(relax2)
+
+    #add 10 years to grad year
+    def after_grad(x):
+        after = x + 10
+        return after
+    #set variable for above function
+    ag = after_grad(age)
+    
 
 
 
