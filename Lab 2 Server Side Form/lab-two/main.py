804212e4234483c508e0996da4a66d201345fb5d
azaimes <azaimes@fullsail.edu>
2014-08-14 14:47:07 -0400

    added body comments and css

--- Lab 2 Server Side Form/lab-two/main.py
+++ Lab 2 Server Side Form/lab-two/main.py
@@ -29,24 +29,43 @@ class MainHandler(webapp2.RequestHandler):
 <html>
     <head>
         <title>Lab 2 Server Side Form</title>
+        <link type="text/css" rel="stylesheet" href="/css/style.css"   />
     </head>
-    <body>'''
-        page_body = '''<form methord="GET">
+    <body>
+        <div class="wrapper">'''
+        #provides the form input
+        page_form = '''<form method="GET">
+
             <fieldset>
-                <label>Name</label><input type="text" name="user" />
-                <label>Email</label><input type="text" name="email" /><br>
-                <label>Address</label><input type="text" name="address" />
-                <label>City</label><input type="text" name="city" />
-                <label>State</label><input type="text" name="state" /><br>
-                <label>Receive mailing</label><select name="mail">
+                <h1>Signup for Classy Coders Today!</h1>
+                <label class="field">Name</label><input class="textBox" type="text" name="user" /><br>
+                <label class="field">Email</label><input class="textBox" type="text" name="email" /><br>
+                <label class="field">Address</label><input class="textBox" type="text" name="address" /><br>
+                <label class="field">City</label><input class="textBox" type="text" name="city" /><br>
+                <label class="field">State</label><input class="textBox" type="text" name="state" /><br>
+                <label class="field">Zip Code</label><input class="textBox" type="text" name="zip" /><br>
+                <label class="mail">Receive mailing</label><select name="mail">
                     <option value="yes">Yes</option>
                     <option value="no">No</option>
-                    </select>
-                <label>Agree to terms</label><input type="checkbox" name="terms" value="agree" />
+                    </select><br>
+                <label class="mail">Agree to terms</label><input type="checkbox" name="terms" value="agree" /><br>
+                <div class="submitButton">
+                <input type="submit" value="Submit" /><br>
+                </div>
             </fieldset>
+        '''
+        # output is presented here after form submission
+        page_body = '''
+        <div class="top"><h1> Thank you, {user}</h1>
+            <h2>Your information has been saved!</h2>
+            <p>Name: {user}</p>
+            <p>Email: {email}</p>
+            <p>Address: {address} {city}, {state}
+        </div>
         '''
         page_close = '''
         </form>
+        </wrapper>
     </body>
 </html>'''
         if self.request.GET:
@@ -55,9 +74,13 @@ class MainHandler(webapp2.RequestHandler):
             address = self.request.GET['address']
             city = self.request.GET['city']
             state = self.request.GET['state']
-            self.response.write(heading + user + ' ' + email + ' ' + page_body + page_close)
-        else:
+            zip = self.request.GET['zip']
+            mail = self.request.GET['mail']
+            terms = self.request.GET['terms']
+            page_body = page_body.format(**locals())
             self.response.write(heading + page_body + page_close)
+        else:
+            self.response.write(heading + page_form + page_close)
 
 
 app = webapp2.WSGIApplication([
