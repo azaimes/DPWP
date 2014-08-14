c632d154a4d619ab0afbaf476a4243f4dde66c95
azaimes <azaimes@fullsail.edu>
2014-08-14 11:25:31 -0400

    completed form

--- Lab 2 Server Side Form/lab-two/main.py
+++ Lab 2 Server Side Form/lab-two/main.py
@@ -32,15 +32,33 @@ class MainHandler(webapp2.RequestHandler):
     </head>
     <body>'''
         page_body = '''<form methord="GET">
-            <label>Name</label><input type="text" name="user" />
-            <label>Email</label><input type="text" name="email" /><br>
-            <label>Address</label><input type="text" name="address" />
-            <label>City</label><input type="text" name="city" />
-            <label>State</label><input type="text" name="state" /><br>
+            <fieldset>
+                <label>Name</label><input type="text" name="user" />
+                <label>Email</label><input type="text" name="email" /><br>
+                <label>Address</label><input type="text" name="address" />
+                <label>City</label><input type="text" name="city" />
+                <label>State</label><input type="text" name="state" /><br>
+                <label>Receive mailing</label><select name="mail">
+                    <option value="yes">Yes</option>
+                    <option value="no">No</option>
+                    </select>
+                <label>Agree to terms</label><input type="checkbox" name="terms" value="agree" />
+            </fieldset>
         '''
         page_close = '''
+        </form>
     </body>
 </html>'''
+        if self.request.GET:
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+            address = self.request.GET['address']
+            city = self.request.GET['city']
+            state = self.request.GET['state']
+            self.response.write(heading + user + ' ' + email + ' ' + page_body + page_close)
+        else:
+            self.response.write(heading + page_body + page_close)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
