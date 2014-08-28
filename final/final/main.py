011382f4970e2da5d2f3c23d1dfc9f47743f3490
azaimes <azaimes@fullsail.edu>
2014-08-28 16:18:47 -0400

    added to second class

--- final/final/main.py
+++ final/final/main.py
@@ -33,6 +33,11 @@ class HouseView(object):
         self.__housedo = []
         self.__content = '<br/>'
 
+    def update(self):
+        for do in self.__housedo:
+            self.__content += "<p>Showing results for houses in: <br/><h2>" + do.city + ", " + do.state + "</h2></p>"
+            self.__content += '<p>Find all the essential data on homes within your search area using the links below.</p><p><ul><li><a href="' + do.for_sale + '">For Sale</a></li><li><a href="' + do.owner_sale + '">For Sale By Owner</a></li><li><a href="' + do.foreclosure + '">Foreclosures</a></li><li><a href="' + do.recently_sold + '">Recently Sold</a></li><li><a href="' + do.affordability + '">Area Affordability</a></li><p class="footer">Home Value In This Area: <strong>' + do.home_value + '</strong><br/>Property Tax In This Area: <strong>' + do.property_tax + '</strong></p>'
+
 
 
 #don't ever mess with this
