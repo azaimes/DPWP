3d4a08266428b5117bf768ff4a4e6edb1574ca7d
azaimes <azaimes@fullsail.edu>
2014-08-28 16:10:54 -0400

    if statement

--- final/final/main.py
+++ final/final/main.py
@@ -9,10 +9,19 @@ import urllib2  # python classes and code needed to requesting info, receiving, 
 from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self): # Controller that collects and sends data to  view model
+    def get(self):  # Controller that collects and sends data to  view model
         p = FormPage()
-        p.inputs = [['city', 'text', 'City'],['state', 'text', 'State'],['Submit', 'submit']]
+        p.inputs = [['city', 'text', 'City'], ['state', 'text', 'State'], ['Submit', 'submit']]
 
+        if self.request.GET: #only if there is a city and state in the url
+            #get info from the API and creates model
+            hm = HouseModel()
+            hm.city = self.request.GET['city']
+            hm.state = self.request.GET['state']
+            hm.callApi()
+
+
+            
 #don't ever mess with this
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
