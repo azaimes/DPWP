7568d79f7c90172e265a4f8c4e359bca22f6b914
azaimes <azaimes@fullsail.edu>
2014-08-28 16:42:28 -0400

    added class

--- final/final/main.py
+++ final/final/main.py
@@ -130,6 +130,32 @@ class HouseData(object):
         return self._head + self._body + self._close
 
 
+class FormPage(Page):
+    def __init__(self):
+        super(FormPage, self).__init__()
+        self._form_open = '<form method="GET">'
+        self._form_close = '</form>'
+        self.__inputs = []
+        self._form_inputs = ''
+
+    @property
+    def inputs(self):
+        pass
+
+    @inputs.setter
+    def inputs(self, arr):
+        self.__inputs = arr
+        for item in arr:
+            self._form_inputs += '<input type="' + item[1] + '" name="' + item[0]
+            try:
+                self._form_inputs += '" placeholder="' + item[2]+'" />'
+            except:
+                self._form_inputs += '" />'
+
+    def print_out(self):
+        return self._head + "<h1>Find Your Dream Home</h1><p>Fill out the city and state you want to look in:</p>" + self._form_open + self._form_inputs + self._form_close + self._body +  self._close
+
+
 #don't ever mess with this
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
