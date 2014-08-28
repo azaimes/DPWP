92a3e54c1b7442383abbe038d9d154b051f35e48
azaimes <azaimes@fullsail.edu>
2014-08-28 16:09:32 -0400

    start of first class

--- final/final/main.py
+++ final/final/main.py
@@ -5,11 +5,15 @@ Class: DPWP
 Assignment: Final
 '''
 import webapp2
+import urllib2  # python classes and code needed to requesting info, receiving, and opening
+from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+    def get(self): # Controller that collects and sends data to  view model
+        p = FormPage()
+        p.inputs = [['city', 'text', 'City'],['state', 'text', 'State'],['Submit', 'submit']]
 
+#don't ever mess with this
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
